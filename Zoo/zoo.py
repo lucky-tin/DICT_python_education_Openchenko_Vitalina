6803c928101ae979da7aba0b1109d808591bfb53
lucky-tin <v.v.openchenko@student.khai.edu>
2023-10-30 22:08:46 +0200

    Zoo 2-nd stage

--- Zoo/zoo.py
+++ Zoo/zoo.py
@@ -4,3 +4,26 @@ print('I love animals!')
 print("Let's check out the animals...")
 print("The deer looks fine.")
 print("The lion looks healthy.")
+
+#Zoo 2-nd stage
+
+camel = r"""
+The camel habitat...
+ ___.-''''-.
+/___ @     |
+',,,,.     |       _.'''''''._
+     '     |     /             \
+     |     \ _.-'               \
+     |                           '.-' '-.
+     |                                   ',
+     |                                     '',
+     ',,-,                                 ':;
+          ',,| ;,,                       ,' ;;
+             ! ;  !'',,,',',,,,'!       ;   ;:
+            : ;    ! !      ! !  ;     ;    :;
+             ; ;    ! !     ! !   ;   ;     ;,
+            ; ;     ! !    ! !     ; ;
+            ; ;     ! !   ! !       ; ;
+           ;,,       !,!  !,!       ;,;
+           /_I       L_I  L_I       /_I
+Look at that!"""
