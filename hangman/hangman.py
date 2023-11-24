1b87601901da70bc54ecbbf40ea79630c07d5dd8
lucky-tin <v.v.openchenko@student.khai.edu>
2023-11-24 15:30:31 +0200

    Hangman 5-th stage

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -15,10 +15,14 @@ def play_hangman():
         print('Hangman')
         choice = input('Type "play" to play the game, "exit" to quit: > ')
 
-        if chice =="play":
+        if choice =="play":
             secret_word = choose_word()
             guessed_word = ["_"] * len(secret_word)
             attempts = 8 #number of attempts
             guessed_letters = [] #list of letters already guessed
 
             print('Welcome to the game!')
+
+            while attempts > 0:
+                print("".join(guessed_word))
+                guess = input("Input a letter : > ").lower()
