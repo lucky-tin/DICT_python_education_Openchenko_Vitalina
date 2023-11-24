6994053b4791131ee4f02377d559cd57b5341b38
lucky-tin <v.v.openchenko@student.khai.edu>
2023-11-24 15:23:35 +0200

    Hangman 3-rd stage

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -8,4 +8,11 @@ word_list= ["python", "java", "javascript", "php"]
 def choose_word():
     return random.choice(word_list)
 
+#game code
+
+def play_hangman():
+    while True:
+        print('Hangman')
+        choice = input('Type "play" to play the game, "exit" to quit: > ')
+
 print('HANGMAN')
