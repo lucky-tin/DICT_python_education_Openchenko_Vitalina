8157ba4235d728c225f6ed6a154daf0113fa70c1
lucky-tin <v.v.openchenko@student.khai.edu>
2023-11-29 20:17:26 +0200

    dinnerparty 2-nd stage

--- dinnerparty/dinnerparty.py
+++ dinnerparty/dinnerparty.py
@@ -11,3 +11,24 @@ else:
     for i in range(num_of_friends):
         friend_name = input()
         friends[friend_name] = 0
+
+    total_amount = float(input("Enter the total amount:\n"))
+
+    if num_of_friends > 0:
+        amount_per_friend = round(total_amount / num_of_friends, 2)
+    else:
+        amount_per_friend = 0
+
+    for friend in friends:
+        friends[friend] = amount_per_friend
+
+    lucky_choice = input("Do you want to use the 'Who is lucky?' feature? Write Yes/No:\n")
+
+    if lucky_choice.lower() == "yes":
+        lucky_friend = random.choice(list(friends.keys()))
+
+        for friend in friends:
+            if friend != lucky_friend:
+                friends[friend] = round(friends[friend] + amount_per_friend, 2)
+
+        friends[lucky_friend] = 0
