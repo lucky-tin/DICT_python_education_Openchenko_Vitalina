5ac3c6f7d5a5275d0239c22ba2872daec3d10cb9
unknown <v.v.openchenko@student.khai.edu>
2024-02-17 14:46:28 +0200

    plw

--- pencils_game/pencils_game.py
+++ pencils_game/pencils_game.py
@@ -2,62 +2,69 @@
 
 import random
 
-
-def is_winning_position(num_pencils):
-    return (num_pencils - 1) % 4 == 0
-
-
-def bot_move(num_pencils):
-    if is_winning_position(num_pencils):
-        return random.randint(1, 3)
-    else:
-        return num_pencils % 4
-
-
-def start_pencil_game():
+def get_pencils():
     while True:
-        try:
-            num_pencils = int(input("How many pencils would you like to use:\n> "))
-            if num_pencils <= 0:
-                print("The number of pencils should be positive")
-            else:
-                break
-        except ValueError:
-            print("The number of pencils should be numeric")
+        pencils = input("How many pencils would you like to use:\n> ")
+        if pencils.isdigit() and int(pencils) > 0:
+            return int(pencils)
+        print("The number of pencils should be numeric and positive")
 
+
+def get_first_player():
     while True:
-        player1_name = input("Who will be the first (Enter a name): ")
-        if player1_name.lower() not in ['john', 'jack']:
-            print("Choose between 'John' and 'Jack'")
+        first_player = input("Who will be the first (John, Jack):\n> ")
+        if first_player in ["John", "Jack"]:
+            return first_player
+        print("Choose between 'John' and 'Jack'")
+
+
+def print_pencils(pencils):
+    print('|' * pencils)
+
+
+def bot_turn(pencils):
+    if pencils == 1:
+        return 1
+    elif pencils % 4 == 1:
+        return  random.randint(1, 3)
+    elif pencils % 4 == 0:
+        return 3
+    elif pencils % 4 == 2:
+        return 1
+    elif pencils % 4 == 3:
+        return 2
+
+
+def player_turn(pencils, current_player):
+    while True:
+        taken = input(f"{current_player}'s turn:\n> ")
+        if taken in ["1", "2", "3"] and int(taken) <= pencils:
+            return int(taken)
+        elif taken not in ["1", "2", "3"]:
+            print("Possible values: '1', '2' or '3'")
         else:
-            break
+            print("Too many pencils were taken")
 
-    current_player = player1_name
-    next_player = 'Jack' if player1_name == 'John' else 'John'
 
-    while num_pencils > 0:
-        print("|" * num_pencils)
-        print(f"{current_player}'s turn:")
+def game():
+    pencils = get_pencils()
+    current_player = get_first_player()
+    print_pencils(pencils)
 
-        if current_player == 'Jack':
-            pencils_taken = bot_move(num_pencils)
-            print(pencils_taken)
+    while pencils > 0:
+        if current_player == "Jack":
+            taken = bot_turn(pencils)
+            print(f"Jack's turn:\n{taken}")
         else:
-            while True:
-                try:
-                    pencils_taken = int(input("> "))
-                    if pencils_taken < 1 or pencils_taken > 3 or pencils_taken > num_pencils:
-                        print("Possible values: '1', '2' or '3'")
-                    else:
-                        break
-                except ValueError:
-                    print("Possible values: '1', '2' or '3'")
+            taken = player_turn(pencils, current_player)
 
-        num_pencils -= pencils_taken
+        pencils -= taken
+        print_pencils(pencils)
 
-        current_player, next_player = next_player, current_player
+        current_player = "John" if current_player == "Jack" else "Jack"
 
-    print(f"{current_player} won!")
+        if pencils == 0:
+            print(f"{current_player} won!")
 
 
-start_pencil_game()
+game()
